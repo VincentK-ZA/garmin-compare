6790858b317ac8cfbb395178d288fb21d2cf40ad
VincentK-ZA <vincent.v.kruger@gmail.com>
2022-01-07 16:55:46 +0200

    Add headers to search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
+import WatchIcon from '@mui/icons-material/Watch';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
+// import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -37,7 +37,6 @@ Album.propTypes = {
 };
 
 export default function Album({ searchData }) {
-  console.log(searchData);
   const watches = searchData ? searchData.hits : [];
 
   watches.sort((a, b) => {
@@ -60,7 +59,7 @@ export default function Album({ searchData }) {
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
+          <WatchIcon sx={{ mr: 2 }} />
           <Typography variant="h6" color="inherit" noWrap>
             Garmin Watch Finder
           </Typography>
@@ -88,10 +87,10 @@ export default function Album({ searchData }) {
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
               Quickly find and compare all of Garmin&apos;s watches
             </Typography>
-            <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
+            {/* <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
               <Button variant="contained">Main call to action</Button>
               <Button variant="outlined">Secondary action</Button>
-            </Stack>
+            </Stack> */}
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
